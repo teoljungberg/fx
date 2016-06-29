ba3943390bc1826d3348b6b4efcf4f290022c3d8
Teo Ljungberg <teo@teoljungberg.com>
2016-06-29 16:20:50 +0200

    Extract `Fx::CommandRecorder::Function`

--- lib/fx/command_recorder.rb
+++ lib/fx/command_recorder.rb
@@ -1,30 +1,9 @@
 require "fx/command_recorder/arguments"
+require "fx/command_recorder/function"
 
 module Fx
   module CommandRecorder
-    def create_function(*args)
-      record(:create_function, args)
-    end
-
-    def drop_function(*args)
-      record(:drop_function, args)
-    end
-
-    def update_function(*args)
-      record(:update_function, args)
-    end
-
-    def invert_create_function(args)
-      [:drop_function, args]
-    end
-
-    def invert_drop_function(args)
-      perform_inversion(:create_function, args)
-    end
-
-    def invert_update_function(args)
-      perform_inversion(:update_function, args)
-    end
+    include Function
 
     private
 
