e2f28e8a81a7f23511b3113797e598317176de97
Teo Ljungberg <teo@teoljungberg.com>
2016-08-03 14:25:38 +0200

    Remove `ignored?` methods for `Fx::SchemaDumper::*`
    The query used to select with functions/triggers we want to dump to the
    schema are already taking care of ignoring unwanted functions/triggers.

--- lib/fx/schema_dumper/trigger.rb
+++ lib/fx/schema_dumper/trigger.rb
@@ -24,23 +24,6 @@ module Fx
       def dumpable_triggers_in_database
         @_dumpable_triggers_in_database ||= Fx.database.triggers
       end
-
-      unless ActiveRecord::SchemaDumper.instance_methods(false).include?(:ignored?)
-        # This method will be present in Rails 4.2.0 and can be removed then.
-        def ignored?(table_name)
-          ["schema_migrations", ignore_tables].flatten.any? do |ignored|
-            case ignored
-            when String; remove_prefix_and_suffix(table_name) == ignored
-            when Regexp; remove_prefix_and_suffix(table_name) =~ ignored
-            else
-              raise(
-                StandardError,
-                "ActiveRecordDumper.ignore_tables accepts an array of String and / or Regexp values.",
-              )
-            end
-          end
-        end
-      end
     end
   end
 end
