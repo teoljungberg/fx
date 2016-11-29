a13f2303fc5d501d90117cb22b617bd7689a4e94
Teo Ljungberg <teo@teoljungberg.com>
2016-11-29 19:33:09 +0100

    Add unit test coverage for `Fx::Adapters::Functions`

---
+++ spec/fx/adapters/postgres/functions_spec.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+
+module Fx
+  module Adapters
+    describe Postgres::Functions, :db do
+      describe ".all" do
+        it "returns `Function` objects" do
+          connection = ActiveRecord::Base.connection
+          connection.execute <<-EOS.strip_heredoc
+            CREATE OR REPLACE FUNCTION test()
+            RETURNS text AS $$
+            BEGIN
+                RETURN 'test';
+            END;
+            $$ LANGUAGE plpgsql;
+          EOS
+
+          functions = Postgres::Functions.new(connection).all
+
+          first = functions.first
+          expect(functions.size).to eq 1
+          expect(first.name).to eq "test"
+          expect(first.definition).to eq <<-EOS.strip_heredoc
+            CREATE OR REPLACE FUNCTION public.test()
+             RETURNS text
+             LANGUAGE plpgsql
+            AS $function$
+            BEGIN
+                RETURN 'test';
+            END;
+            $function$
+          EOS
+        end
+      end
+    end
+  end
+end
