24b0dbe29cc5a5fdae6c6f7f30b515a8c521cb21
Guilherme Cortes <guilhermecortes@gmail.com>
2019-09-13 15:01:52 +0200

    Fix nested modules/classes

--- spec/fx/adapters/postgres_spec.rb
+++ spec/fx/adapters/postgres_spec.rb
@@ -1,80 +1,10 @@
 require "spec_helper"
 
-module Fx::Adapters
-  describe Postgres, :db do
-    describe "#create_function" do
-      it "successfully creates a function" do
-        adapter = Postgres.new
-        adapter.create_function(
-          <<-SQL,
-            CREATE OR REPLACE FUNCTION test()
-            RETURNS text AS $$
-            BEGIN
-                RETURN 'test';
-            END;
-            $$ LANGUAGE plpgsql;
-          SQL
-        )
-
-        expect(adapter.functions.map(&:name)).to include("test")
-      end
-    end
-
-    describe "#create_trigger" do
-      it "successfully creates a trigger" do
-        connection.execute <<-SQL
-          CREATE TABLE users (
-              id int PRIMARY KEY,
-              name varchar(256),
-              upper_name varchar(256)
-          );
-        SQL
-        adapter = Postgres.new
-        adapter.create_function <<-SQL
-          CREATE OR REPLACE FUNCTION uppercase_users_name()
-          RETURNS trigger AS $$
-          BEGIN
-            NEW.upper_name = UPPER(NEW.name);
-            RETURN NEW;
-          END;
-          $$ LANGUAGE plpgsql;
-        SQL
-        adapter.create_trigger(
-          <<-SQL,
-            CREATE TRIGGER uppercase_users_name
-                BEFORE INSERT ON users
-                FOR EACH ROW
-                EXECUTE PROCEDURE uppercase_users_name();
-          SQL
-        )
-
-        expect(adapter.triggers.map(&:name)).to include("uppercase_users_name")
-      end
-    end
-
-    describe "#drop_function" do
-      context "when the function has arguments" do
-        it "successfully drops a function with the entire function signature" do
-          adapter = Postgres.new
-          adapter.create_function(
-            <<-SQL,
-              CREATE FUNCTION adder(x int, y int)
-              RETURNS int AS $$
-              BEGIN
-                  RETURN $1 + $2;
-              END;
-              $$ LANGUAGE plpgsql;
-            SQL
-          )
-
-          adapter.drop_function(:adder)
-
-          expect(adapter.functions.map(&:name)).not_to include("adder")
-        end
-      end
-
-      context "when the function does not have arguments" do
-        it "successfully drops a function" do
+module Fx
+  module Adapters
+    describe Postgres, :db do
+      describe "#create_function" do
+        it "successfully creates a function" do
           adapter = Postgres.new
           adapter.create_function(
             <<-SQL,
@@ -87,59 +17,131 @@ module Fx::Adapters
             SQL
           )
 
-          adapter.drop_function(:test)
-
-          expect(adapter.functions.map(&:name)).not_to include("test")
+          expect(adapter.functions.map(&:name)).to include("test")
         end
       end
-    end
 
-    describe "#functions" do
-      it "finds functions and builds Fx::Function objects" do
-        adapter = Postgres.new
-        adapter.create_function(
-          <<-SQL,
-            CREATE OR REPLACE FUNCTION test()
-            RETURNS text AS $$
+      describe "#create_trigger" do
+        it "successfully creates a trigger" do
+          connection.execute <<-SQL
+            CREATE TABLE users (
+                id int PRIMARY KEY,
+                name varchar(256),
+                upper_name varchar(256)
+            );
+          SQL
+          adapter = Postgres.new
+          adapter.create_function <<-SQL
+            CREATE OR REPLACE FUNCTION uppercase_users_name()
+            RETURNS trigger AS $$
             BEGIN
-                RETURN 'test';
+              NEW.upper_name = UPPER(NEW.name);
+              RETURN NEW;
             END;
             $$ LANGUAGE plpgsql;
           SQL
-        )
+          adapter.create_trigger(
+            <<-SQL,
+              CREATE TRIGGER uppercase_users_name
+                  BEFORE INSERT ON users
+                  FOR EACH ROW
+                  EXECUTE PROCEDURE uppercase_users_name();
+            SQL
+          )
 
-        expect(adapter.functions.map(&:name)).to eq ["test"]
+          expect(adapter.triggers.map(&:name)).to include("uppercase_users_name")
+        end
       end
-    end
 
-    describe "#triggers" do
-      it "finds triggers and builds Fx::Trigger objects" do
-        connection.execute <<-SQL
-          CREATE TABLE users (
-              id int PRIMARY KEY,
-              name varchar(256),
-              upper_name varchar(256)
-          );
-        SQL
-        adapter = Postgres.new
-        adapter.create_function <<-SQL
-          CREATE OR REPLACE FUNCTION uppercase_users_name()
-          RETURNS trigger AS $$
-          BEGIN
-            NEW.upper_name = UPPER(NEW.name);
-            RETURN NEW;
-          END;
-          $$ LANGUAGE plpgsql;
-        SQL
-        sql_definition = <<-SQL
-          CREATE TRIGGER uppercase_users_name
-              BEFORE INSERT ON users
-              FOR EACH ROW
-              EXECUTE PROCEDURE uppercase_users_name()
-        SQL
-        adapter.create_trigger(sql_definition)
+      describe "#drop_function" do
+        context "when the function has arguments" do
+          it "successfully drops a function with the entire function signature" do
+            adapter = Postgres.new
+            adapter.create_function(
+              <<-SQL,
+                CREATE FUNCTION adder(x int, y int)
+                RETURNS int AS $$
+                BEGIN
+                    RETURN $1 + $2;
+                END;
+                $$ LANGUAGE plpgsql;
+              SQL
+            )
 
-        expect(adapter.triggers.map(&:name)).to eq ["uppercase_users_name"]
+            adapter.drop_function(:adder)
+
+            expect(adapter.functions.map(&:name)).not_to include("adder")
+          end
+        end
+
+        context "when the function does not have arguments" do
+          it "successfully drops a function" do
+            adapter = Postgres.new
+            adapter.create_function(
+              <<-SQL,
+                CREATE OR REPLACE FUNCTION test()
+                RETURNS text AS $$
+                BEGIN
+                    RETURN 'test';
+                END;
+                $$ LANGUAGE plpgsql;
+              SQL
+            )
+
+            adapter.drop_function(:test)
+
+            expect(adapter.functions.map(&:name)).not_to include("test")
+          end
+        end
+      end
+
+      describe "#functions" do
+        it "finds functions and builds Fx::Function objects" do
+          adapter = Postgres.new
+          adapter.create_function(
+            <<-SQL,
+              CREATE OR REPLACE FUNCTION test()
+              RETURNS text AS $$
+              BEGIN
+                  RETURN 'test';
+              END;
+              $$ LANGUAGE plpgsql;
+            SQL
+          )
+
+          expect(adapter.functions.map(&:name)).to eq ["test"]
+        end
+      end
+
+      describe "#triggers" do
+        it "finds triggers and builds Fx::Trigger objects" do
+          connection.execute <<-SQL
+            CREATE TABLE users (
+                id int PRIMARY KEY,
+                name varchar(256),
+                upper_name varchar(256)
+            );
+          SQL
+          adapter = Postgres.new
+          adapter.create_function <<-SQL
+            CREATE OR REPLACE FUNCTION uppercase_users_name()
+            RETURNS trigger AS $$
+            BEGIN
+              NEW.upper_name = UPPER(NEW.name);
+              RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+          SQL
+          sql_definition = <<-SQL
+            CREATE TRIGGER uppercase_users_name
+                BEFORE INSERT ON users
+                FOR EACH ROW
+                EXECUTE PROCEDURE uppercase_users_name()
+          SQL
+          adapter.create_trigger(sql_definition)
+
+          expect(adapter.triggers.map(&:name)).to eq ["uppercase_users_name"]
+        end
       end
     end
   end
--- spec/fx/command_recorder/arguments_spec.rb
+++ spec/fx/command_recorder/arguments_spec.rb
@@ -1,40 +1,42 @@
 require "spec_helper"
 
-module Fx::CommandRecorder
-  describe Arguments do
-    describe "#function" do
-      it "returns the function name" do
-        raw_args = [:spaceships, { foo: :bar }]
-        args = Arguments.new(raw_args)
+module Fx
+  module CommandRecorder
+    describe Arguments do
+      describe "#function" do
+        it "returns the function name" do
+          raw_args = [:spaceships, { foo: :bar }]
+          args = Arguments.new(raw_args)
 
-        expect(args.function).to eq :spaceships
-      end
-    end
-
-    describe "#revert_to_version" do
-      it "is the revert_to_version from the keyword arguments" do
-        raw_args = [:spaceships, { revert_to_version: 42 }]
-        args = Arguments.new(raw_args)
-
-        expect(args.revert_to_version).to eq 42
+          expect(args.function).to eq :spaceships
+        end
       end
 
-      it "is nil if the revert_to_version was not supplied" do
-        raw_args = [:spaceships, { foo: :bar }]
-        args = Arguments.new(raw_args)
+      describe "#revert_to_version" do
+        it "is the revert_to_version from the keyword arguments" do
+          raw_args = [:spaceships, { revert_to_version: 42 }]
+          args = Arguments.new(raw_args)
 
-        expect(args.revert_to_version).to be nil
+          expect(args.revert_to_version).to eq 42
+        end
+
+        it "is nil if the revert_to_version was not supplied" do
+          raw_args = [:spaceships, { foo: :bar }]
+          args = Arguments.new(raw_args)
+
+          expect(args.revert_to_version).to be nil
+        end
       end
-    end
 
-    describe "#invert_version" do
-      it "returns object with version set to revert_to_version" do
-        raw_args = [:meatballs, { version: 42, revert_to_version: 15 }]
+      describe "#invert_version" do
+        it "returns object with version set to revert_to_version" do
+          raw_args = [:meatballs, { version: 42, revert_to_version: 15 }]
 
-        inverted_args = Arguments.new(raw_args).invert_version
+          inverted_args = Arguments.new(raw_args).invert_version
 
-        expect(inverted_args.version).to eq 15
-        expect(inverted_args.revert_to_version).to be nil
+          expect(inverted_args.version).to eq 15
+          expect(inverted_args.revert_to_version).to be nil
+        end
       end
     end
   end
