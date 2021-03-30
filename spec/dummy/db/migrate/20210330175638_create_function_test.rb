db63da766d8390cd52d404d2ba0b8a6a5f05b97a
Nick Martin <nick.martin@mirah.com>
2021-03-30 15:12:38 -0400

    These should not have been comitted.

--- spec/dummy/db/migrate/20210330175638_create_function_test.rb
+++
@@ -1,5 +0,0 @@
-class CreateFunctionTest < ActiveRecord::Migration[6.1]
-  def change
-    create_function :test
-  end
-end
--- spec/dummy/db/migrate/20210330175641_update_function_test_to_version_2.rb
+++
@@ -1,5 +0,0 @@
-class UpdateFunctionTestToVersion2 < ActiveRecord::Migration[6.1]
-  def change
-    update_function :test, version: 2, revert_to_version: 1
-  end
-end
