3a29662fae64f4cabdab365d1ece859710403f07
Teo Ljungberg <teo@teoljungberg.com>
2023-01-21 13:00:38 +0100

    Configure `active_record.legacy_connection_handling` (#105)

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -11,5 +11,7 @@ module Dummy
     config.cache_classes = true
     config.eager_load = false
     config.active_support.deprecation = :stderr
+
+    config.active_record.legacy_connection_handling = false
   end
 end
