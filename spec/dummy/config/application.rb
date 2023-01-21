ad2d90156e42acfc5c8cf6f435b420a116344368
Teo Ljungberg <teo@teoljungberg.com>
2023-01-21 12:29:00 +0100

    Configure `active_record.legacy_connection_handling`

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -11,5 +11,9 @@ module Dummy
     config.cache_classes = true
     config.eager_load = false
     config.active_support.deprecation = :stderr
+
+    if Rails.gem_version >= Gem::Version.new("6.1") && Rails.gem_version <= Gem::Version.new("7.0")
+      config.active_record.legacy_connection_handling = false
+    end
   end
 end
