4ffb2796ae33fe9169db831ca88dcfc7020b0b86
Teo Ljungberg <teo@teoljungberg.com>
2023-01-21 12:20:09 +0100

    Set Rails.root (#103)
    This directory is cleaned out, and in tmp is where my postgresql storage
    lives.

--- spec/support/generator_setup.rb
+++ spec/support/generator_setup.rb
@@ -2,7 +2,7 @@ require "ammeter/init"
 
 RSpec.configure do |config|
   config.before(:each, :generator) do
-    fake_rails_root = File.expand_path("../../../tmp", __FILE__)
+    fake_rails_root = File.expand_path("../../../tmp/dummy", __FILE__)
     allow(Rails).to receive(:root).and_return(Pathname.new(fake_rails_root))
 
     destination fake_rails_root
