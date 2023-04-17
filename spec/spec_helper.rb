f3a935a0b82a68971e83e98b5bc0d5c101d04552
Teo Ljungberg <teo@teoljungberg.com>
2023-04-17 20:30:03 +0200

    Move requires

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,9 @@ require "database_cleaner"
 require File.expand_path("../dummy/config/environment", __FILE__)
 Dir["spec/support/**/*.rb"].sort.each { |file| load file }
 
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "fx"
+
 RSpec.configure do |config|
   config.order = "random"
   config.disable_monkey_patching!
