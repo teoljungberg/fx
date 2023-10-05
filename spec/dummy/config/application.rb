ed8524e93827d127d3a7e36d643c91897e63f7d7
Teo Ljungberg <teo@teoljungberg.com>
2023-10-05 11:18:52 +0200

    Fix deprecation warning
    https://guides.rubyonrails.org/upgrading_ruby_on_rails.html#new-activesupport-cache-serialization-format

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -12,6 +12,8 @@ module Dummy
     config.eager_load = false
     config.active_support.deprecation = :stderr
 
+    config.load_defaults 7.0
+
     if Rails.version <= "7.0"
       config.active_record.legacy_connection_handling = false
     end
