1dc92de45fbfbfbed9b5895821cc79c19175f2bc
Teo Ljungberg <teo@teoljungberg.com>
2024-05-25 09:39:53 +0200

    Inline CommandRecorder::{Function,Trigger}

--- lib/fx/command_recorder.rb
+++ lib/fx/command_recorder.rb
@@ -1,12 +1,55 @@
 require "fx/command_recorder/arguments"
-require "fx/command_recorder/function"
-require "fx/command_recorder/trigger"
 
 module Fx
   # @api private
   module CommandRecorder
-    include Function
-    include Trigger
+    def create_function(*args)
+      record(:create_function, args)
+    end
+
+    def drop_function(*args)
+      record(:drop_function, args)
+    end
+
+    def update_function(*args)
+      record(:update_function, args)
+    end
+
+    def invert_create_function(args)
+      [:drop_function, args]
+    end
+
+    def invert_drop_function(args)
+      perform_inversion(:create_function, args)
+    end
+
+    def invert_update_function(args)
+      perform_inversion(:update_function, args)
+    end
+
+    def create_trigger(*args)
+      record(:create_trigger, args)
+    end
+
+    def drop_trigger(*args)
+      record(:drop_trigger, args)
+    end
+
+    def update_trigger(*args)
+      record(:update_trigger, args)
+    end
+
+    def invert_create_trigger(args)
+      [:drop_trigger, args]
+    end
+
+    def invert_drop_trigger(args)
+      perform_inversion(:create_trigger, args)
+    end
+
+    def invert_update_trigger(args)
+      perform_inversion(:update_trigger, args)
+    end
 
     private
 
