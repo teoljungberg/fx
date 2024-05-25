73755fc8b91a1682499e799d8571a06e9d3e2589
Teo Ljungberg <teo@teoljungberg.com>
2024-05-25 09:44:54 +0200

    Inline SchemaDumper::{Function,Trigger}

--- lib/fx/schema_dumper.rb
+++ lib/fx/schema_dumper.rb
@@ -1,10 +1,50 @@
-require "fx/schema_dumper/function"
-require "fx/schema_dumper/trigger"
-
 module Fx
   # @api private
   module SchemaDumper
-    include Function
-    include Trigger
+    def tables(stream)
+      if Fx.configuration.dump_functions_at_beginning_of_schema
+        functions(stream)
+        empty_line(stream)
+      end
+
+      super
+
+      unless Fx.configuration.dump_functions_at_beginning_of_schema
+        functions(stream)
+        empty_line(stream)
+      end
+
+      triggers(stream)
+    end
+
+    private
+
+    def empty_line(stream)
+      stream.puts if dumpable_functions_in_database.any?
+    end
+
+    def functions(stream)
+      dumpable_functions_in_database.each do |function|
+        stream.puts(function.to_schema)
+      end
+    end
+
+    def triggers(stream)
+      if dumpable_triggers_in_database.any?
+        stream.puts
+      end
+
+      dumpable_triggers_in_database.each do |trigger|
+        stream.puts(trigger.to_schema)
+      end
+    end
+
+    def dumpable_functions_in_database
+      @_dumpable_functions_in_database ||= Fx.database.functions
+    end
+
+    def dumpable_triggers_in_database
+      @_dumpable_triggers_in_database ||= Fx.database.triggers
+    end
   end
 end
