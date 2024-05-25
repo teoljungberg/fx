feeef1994512ee0404de16451a3536976a0550fc
Teo Ljungberg <teo@teoljungberg.com>
2024-05-25 10:33:38 +0200

    Remove faulty documentation

--- lib/fx/adapters/postgres/connection.rb
+++ lib/fx/adapters/postgres/connection.rb
@@ -1,13 +1,6 @@
 module Fx
   module Adapters
     class Postgres
-      # Decorates an ActiveRecord connection with methods that help determine
-      # the connections capabilities.
-      #
-      # Every attempt is made to use the versions of these methods defined by
-      # Rails where they are available and public before falling back to our own
-      # implementations for older Rails versions.
-      #
       # @api private
       class Connection < SimpleDelegator
       end
