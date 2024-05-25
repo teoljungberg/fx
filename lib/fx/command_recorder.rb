870ebfea4e65b04ff2e09ceeb9edb7c162d4fe1b
Teo Ljungberg <teo@teoljungberg.com>
2024-05-25 09:56:35 +0200

    Inline CommandRecorder::Arguments

--- lib/fx/command_recorder.rb
+++ lib/fx/command_recorder.rb
@@ -1,5 +1,3 @@
-require "fx/command_recorder/arguments"
-
 module Fx
   # @api private
   module CommandRecorder
@@ -62,6 +60,45 @@ module Fx
       end
 
       [method, arguments.invert_version.to_a]
+    end
+
+    class Arguments
+      def initialize(args)
+        @args = args.freeze
+      end
+
+      def function
+        @args[0]
+      end
+
+      def version
+        options[:version]
+      end
+
+      def revert_to_version
+        options[:revert_to_version]
+      end
+
+      def invert_version
+        Arguments.new([function, options_for_revert])
+      end
+
+      def to_a
+        @args.to_a
+      end
+
+      private
+
+      def options
+        @options ||= @args[1] || {}
+      end
+
+      def options_for_revert
+        options.clone.tap do |revert_options|
+          revert_options[:version] = revert_to_version
+          revert_options.delete(:revert_to_version)
+        end
+      end
     end
   end
 end
