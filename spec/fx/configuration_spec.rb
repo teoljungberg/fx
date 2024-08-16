3f539deafd3e7095886a846c3dd216ce9098d4d1
Teo Ljungberg <teo@teoljungberg.com>
2024-08-16 10:41:50 +0200

    Refactor configuration tests

--- spec/fx/configuration_spec.rb
+++ spec/fx/configuration_spec.rb
@@ -2,20 +2,31 @@ require "spec_helper"
 
 RSpec.describe Fx::Configuration do
   it "defaults the database adapter to postgres" do
-    expect(Fx.configuration.database).to be_a(Fx::Adapters::Postgres)
-    expect(Fx.database).to be_a(Fx::Adapters::Postgres)
+    configuration = Fx::Configuration.new
+
+    expect(configuration.database).to be_a(Fx::Adapters::Postgres)
+  end
+
+  it "defaults `dump_functions_at_beginning_of_schema` to false" do
+    configuration = Fx::Configuration.new
+
+    expect(configuration.dump_functions_at_beginning_of_schema).to eq(false)
   end
 
   it "allows the database adapter to be set" do
+    configuration = Fx::Configuration.new
     adapter = double("Fx Adapter")
 
-    Fx.configure do |config|
-      config.database = adapter
-    end
+    configuration.database = adapter
 
-    expect(Fx.configuration.database).to eq(adapter)
-    expect(Fx.database).to eq(adapter)
+    expect(configuration.database).to eq(adapter)
+  end
 
-    Fx.configuration = Fx::Configuration.new
+  it "allows `dump_functions_at_beginning_of_schema` to be set" do
+    configuration = Fx::Configuration.new
+
+    configuration.dump_functions_at_beginning_of_schema = true
+
+    expect(configuration.dump_functions_at_beginning_of_schema).to eq(true)
   end
 end
