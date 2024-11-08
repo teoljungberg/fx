67ab982fa5a06b8a65a4ab5bfb9cd5eee3c38918
Teo Ljungberg <teo@teoljungberg.com>
2024-11-08 11:33:30 +0100

    Remove Rails < 7 casing

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -13,9 +13,5 @@ module Dummy
     config.active_support.deprecation = :stderr
 
     config.load_defaults 7.0
-
-    if Rails.version <= "7.0"
-      config.active_record.legacy_connection_handling = false
-    end
   end
 end
