50d617d6d0b783f36ee6ab74cb257db950bad04b
Jake Humphrey <jake@harvestprofit.com>
2025-02-11 14:31:51 -0800

    Fixing 2 tests

--- spec/fx/statements_spec.rb
+++ spec/fx/statements_spec.rb
@@ -46,7 +46,7 @@ RSpec.describe Fx::Statements, :db do
 
       connection.drop_function(:test)
 
-      expect(database).to have_received(:drop_function).with(:test)
+      expect(database).to have_received(:drop_function).with(:test, if_exists: false)
     end
 
     it "drops the function with if_exists" do
@@ -142,7 +142,7 @@ RSpec.describe Fx::Statements, :db do
       connection.drop_trigger(:test, on: :users)
 
       expect(database).to have_received(:drop_trigger)
-        .with(:test, on: :users)
+        .with(:test, on: :users, if_exists: false)
     end
 
     it "drops the trigger with if_exists" do
