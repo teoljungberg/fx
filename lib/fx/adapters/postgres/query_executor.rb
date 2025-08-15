c3637192150258968a0bef675eac5827e0163090
Teo Ljungberg <teo@teoljungberg.com>
2025-08-15 11:04:43 +0200

    Extract QueryExecutor from {Functions,Triggers}.all

---
+++ lib/fx/adapters/postgres/query_executor.rb
@@ -0,0 +1,30 @@
+module Fx
+  module Adapters
+    class Postgres
+      # Executes database queries and maps results to domain objects.
+      # @api private
+      class QueryExecutor
+        def initialize(connection:, query:, model_class:)
+          @connection = connection
+          @query = query
+          @model_class = model_class
+        end
+
+        # Executes the query and maps results to domain objects.
+        #
+        # @return [Array] Array of domain objects (Functions or Triggers)
+        def all
+          results_from_postgres.map { |result| model_class.new(result) }
+        end
+
+        private
+
+        attr_reader :connection, :query, :model_class
+
+        def results_from_postgres
+          connection.execute(query)
+        end
+      end
+    end
+  end
+end
