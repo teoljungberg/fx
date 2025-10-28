010ade79707f24edb3d94a009463535b9dfa1de9
Alex Robbin <agrobbin@gmail.com>
2025-10-27 22:06:56 -0400

    ensure multi-schema dumping in Rails 8.1.0 doesn't dump the same objects for all schemas
    In Rails 8.1.0, `bin/rails db:schema:dump` now knows how to dump multiple schemas (rails/rails#50020). To ensure we don't dump the same functions / triggers for each schema, we need to stop memoizing `Fx.database.{functions,triggers}`.
    
    This should be fully backwards compatible to previous Rails versions, as we still only call each of those methods once per schema dumping invocation, it just now works if schema dumping is invoked multiple times.

--- lib/fx/schema_dumper.rb
+++ lib/fx/schema_dumper.rb
@@ -4,14 +4,12 @@ module Fx
     def tables(stream)
       if Fx.configuration.dump_functions_at_beginning_of_schema
         functions(stream)
-        empty_line(stream)
       end
 
       super
 
       unless Fx.configuration.dump_functions_at_beginning_of_schema
         functions(stream)
-        empty_line(stream)
       end
 
       triggers(stream)
@@ -19,17 +17,19 @@ module Fx
 
     private
 
-    def empty_line(stream)
-      stream.puts if dumpable_functions_in_database.any?
-    end
-
     def functions(stream)
+      dumpable_functions_in_database = Fx.database.functions
+
       dumpable_functions_in_database.each do |function|
         stream.puts(function.to_schema)
       end
+
+      stream.puts if dumpable_functions_in_database.any?
     end
 
     def triggers(stream)
+      dumpable_triggers_in_database = Fx.database.triggers
+
       if dumpable_triggers_in_database.any?
         stream.puts
       end
@@ -37,14 +37,6 @@ module Fx
       dumpable_triggers_in_database.each do |trigger|
         stream.puts(trigger.to_schema)
       end
-    end
-
-    def dumpable_functions_in_database
-      @_dumpable_functions_in_database ||= Fx.database.functions
-    end
-
-    def dumpable_triggers_in_database
-      @_dumpable_triggers_in_database ||= Fx.database.triggers
     end
   end
 end
