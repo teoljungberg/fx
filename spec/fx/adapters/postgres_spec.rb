a077e52ae0ef6e552d7ae36b4a6cb34c5a1b31ce
Teo Ljungberg <teo@teoljungberg.com>
2025-10-31 10:27:18 +0100

    Extract PostgreSQL version constants
    - Extract magic number 10_00_00 into POSTGRES_VERSIONS constant hash
    - Add documentation with links to PostgreSQL release notes
    - Extract server_version private method for better testability
    - Add comprehensive tests for version checking (9.5, 9.6, 10.0, 11.0)
    
    This refactoring improves code readability and makes it easier to add
    support for future PostgreSQL versions.
    
    References:
    - https://www.postgresql.org/docs/10/release-10.html
    - https://www.postgresql.org/docs/11/release-11.html

--- spec/fx/adapters/postgres_spec.rb
+++ spec/fx/adapters/postgres_spec.rb
@@ -141,4 +141,46 @@ RSpec.describe Fx::Adapters::Postgres, :db do
       expect(adapter.triggers.map(&:name)).to eq ["uppercase_users_name"]
     end
   end
+
+  describe "#support_drop_function_without_args" do
+    it "returns true for PostgreSQL version 10.0" do
+      adapter = Fx::Adapters::Postgres.new
+      connection = adapter.send(:connection)
+      allow(connection).to receive(:server_version).and_return(10_00_00)
+
+      result = connection.support_drop_function_without_args
+
+      expect(result).to be(true)
+    end
+
+    it "returns true for PostgreSQL version 11.0" do
+      adapter = Fx::Adapters::Postgres.new
+      connection = adapter.send(:connection)
+      allow(connection).to receive(:server_version).and_return(11_00_00)
+
+      result = connection.support_drop_function_without_args
+
+      expect(result).to be(true)
+    end
+
+    it "returns false for PostgreSQL version 9.6" do
+      adapter = Fx::Adapters::Postgres.new
+      connection = adapter.send(:connection)
+      allow(connection).to receive(:server_version).and_return(9_06_00)
+
+      result = connection.support_drop_function_without_args
+
+      expect(result).to be(false)
+    end
+
+    it "returns false for PostgreSQL version 9.5" do
+      adapter = Fx::Adapters::Postgres.new
+      connection = adapter.send(:connection)
+      allow(connection).to receive(:server_version).and_return(9_05_00)
+
+      result = connection.support_drop_function_without_args
+
+      expect(result).to be(false)
+    end
+  end
 end
