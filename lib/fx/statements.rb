6bf8e2c6eeafc01a6fcdcef81ab7e66eb22b42b7
Teo Ljungberg <teo@teoljungberg.com>
2025-10-31 10:36:54 +0100

    YARD documentation improvements (#179)
    - Add proper YARD type annotations to all public API methods
    - Change Fixnum → Integer (Fixnum is deprecated in modern Ruby)
    - s/prescedence/precedence
    - Add [void] return type annotations where appropriate
    - Fix documentation for update_trigger (was "trigger to drop", should be
      "trigger to update")
    - Improve consistency of documentation format across adapters,
      configuration, and statements

--- lib/fx/statements.rb
+++ lib/fx/statements.rb
@@ -4,13 +4,13 @@ module Fx
     # Create a new database function.
     #
     # @param name [String, Symbol] The name of the database function.
-    # @param version [Fixnum] The version number of the function, used to
+    # @param version [Integer] The version number of the function, used to
     #   find the definition file in `db/functions`. This defaults to `1` if
     #   not provided.
     # @param sql_definition [String] The SQL query for the function schema.
     #   If both `sql_definition` and `version` are provided,
-    #   `sql_definition` takes prescedence.
-    # @return The database response from executing the create statement.
+    #   `sql_definition` takes precedence.
+    # @return [void] The database response from executing the create statement.
     #
     # @example Create from `db/functions/uppercase_users_name_v02.sql`
     #   create_function(:uppercase_users_name, version: 2)
@@ -45,10 +45,10 @@ module Fx
     # Drop a database function by name.
     #
     # @param name [String, Symbol] The name of the database function.
-    # @param revert_to_version [Fixnum] Used to reverse the `drop_function`
+    # @param revert_to_version [Integer] Used to reverse the `drop_function`
     #   command on `rake db:rollback`. The provided version will be passed as
     #   the `version` argument to {#create_function}.
-    # @return The database response from executing the drop statement.
+    # @return [void] The database response from executing the drop statement.
     #
     # @example Drop a function, rolling back to version 2 on rollback
     #   drop_function(:uppercase_users_name, revert_to_version: 2)
@@ -60,13 +60,13 @@ module Fx
     # Update a database function.
     #
     # @param name [String, Symbol] The name of the database function.
-    # @param version [Fixnum] The version number of the function, used to
+    # @param version [Integer] The version number of the function, used to
     #   find the definition file in `db/functions`. This defaults to `1` if
     #   not provided.
     # @param sql_definition [String] The SQL query for the function schema.
     #   If both `sql_definition` and `version` are provided,
-    #   `sql_definition` takes prescedence.
-    # @return The database response from executing the create statement.
+    #   `sql_definition` takes precedence.
+    # @return [void] The database response from executing the create statement.
     #
     # @example Update function to a given version
     #   update_function(
@@ -106,13 +106,13 @@ module Fx
     # Create a new database trigger.
     #
     # @param name [String, Symbol] The name of the database trigger.
-    # @param version [Fixnum] The version number of the trigger, used to
+    # @param version [Integer] The version number of the trigger, used to
     #   find the definition file in `db/triggers`. This defaults to `1` if
     #   not provided.
     # @param sql_definition [String] The SQL query for the function. An error
     #   will be raised if `sql_definition` and `version` are both set,
     #   as they are mutually exclusive.
-    # @return The database response from executing the create statement.
+    # @return [void] The database response from executing the create statement.
     #
     # @example Create trigger from `db/triggers/uppercase_users_name_v01.sql`
     #   create_trigger(:uppercase_users_name, version: 1)
@@ -152,10 +152,10 @@ module Fx
     # @param name [String, Symbol] The name of the database trigger.
     # @param on [String, Symbol] The name of the table the database trigger
     #   is associated with.
-    # @param revert_to_version [Fixnum] Used to reverse the `drop_trigger`
+    # @param revert_to_version [Integer] Used to reverse the `drop_trigger`
     #   command on `rake db:rollback`. The provided version will be passed as
     #   the `version` argument to {#create_trigger}.
-    # @return The database response from executing the drop statement.
+    # @return [void] The database response from executing the drop statement.
     #
     # @example Drop a trigger, rolling back to version 3 on rollback
     #   drop_trigger(:log_inserts, on: :users, revert_to_version: 3)
@@ -171,15 +171,15 @@ module Fx
     # and `version` parameter.
     #
     # @param name [String, Symbol] The name of the database trigger.
-    # @param version [Fixnum] The version number of the trigger.
+    # @param version [Integer] The version number of the trigger.
     # @param on [String, Symbol] The name of the table the database trigger
     #   is associated with.
     # @param sql_definition [String] The SQL query for the function. An error
     #   will be raised if `sql_definition` and `version` are both set,
     #   as they are mutually exclusive.
-    # @param revert_to_version [Fixnum] The version number to rollback to on
+    # @param revert_to_version [Integer] The version number to rollback to on
     #   `rake db rollback`
-    # @return The database response from executing the create statement.
+    # @return [void] The database response from executing the create statement.
     #
     # @example Update trigger to a given version
     #   update_trigger(
